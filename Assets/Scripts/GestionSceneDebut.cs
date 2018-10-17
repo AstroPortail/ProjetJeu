5b4723100c895ce7e31535e3e39d2543f8cb7eb6
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2018-10-17 12:49:40 -0400

    Kimpushe

--- Assets/Scripts/GestionSceneDebut.cs
+++ Assets/Scripts/GestionSceneDebut.cs
@@ -12,26 +12,54 @@ public class GestionSceneDebut: MonoBehaviour {
 
     //public InputField champNomJoueur;
 
-    public void Rejouer()
+   void Start()
     {
-        SceneManager.LoadScene("SceneJeu");
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Update()
+    {
+
+        // Fonction temporaire pour visualiser les divers environnements en appuyant sur une touche
+        if (Input.GetKeyDown("f"))
+        {
+         
+            SceneManager.LoadScene("Automne");
+        }
+
+        if (Input.GetKeyDown("e"))
+        {
+            SceneManager.LoadScene("EteLaurence");
+        }
+
+        if (Input.GetKeyDown("h"))
+        {
+            SceneManager.LoadScene("HiverHelo");
+        }
+    }
+
+
+        public void Rejouer()
+    {
+        SceneManager.LoadScene("HiverHelo");
     }
 
     public void Jouer()
     {   
-        SceneManager.LoadScene("SceneJeu");
+        SceneManager.LoadScene("HiverHelo");
     }
 
     public void Instructions()
     {
         
           PanneauInstru.SetActive(true);
-            btnReglages.SetActive(false);
+          btnReglages.SetActive(false);
           btnInstru.SetActive(false);
           btnJouer.SetActive(true);
 
-
     }
+
+    
 
 
 }// FIN CLASSE GESTIONSCENES
