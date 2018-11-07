15b8ab0f5ba5b652f815802205e61ba898dbbdd8
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2018-11-07 12:24:05 -0500

    countdownStopTemps

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -12,23 +12,28 @@ public class Countdown : MonoBehaviour
 
     private void Update()
     {
-        totalTime -= Time.deltaTime;
-        UpdateLevelTimer(totalTime);
+        if (GestionCamera.pause == false)
+        {
+            totalTime -= Time.deltaTime;
+            UpdateLevelTimer(totalTime);
+        }
     }
 
     public void UpdateLevelTimer(float totalSeconds)
     {
-        int minutes = Mathf.FloorToInt(totalSeconds / 60f);
-        int seconds = Mathf.RoundToInt(totalSeconds % 60f);
+        
+            int minutes = Mathf.FloorToInt(totalSeconds / 60f);
+            int seconds = Mathf.RoundToInt(totalSeconds % 60f);
 
-        string formatedSeconds = seconds.ToString();
+            string formatedSeconds = seconds.ToString();
 
-        if (seconds == 60)
-        {
-            seconds = 0;
-            minutes += 1;
-        }
+            if (seconds == 60)
+            {
+                seconds = 0;
+                minutes += 1;
+            }
 
-        timer.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+            timer.text = minutes.ToString("00") + ":" + seconds.ToString("00");
+        
     }
 }
