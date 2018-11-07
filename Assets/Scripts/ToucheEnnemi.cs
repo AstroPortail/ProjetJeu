ef83574cffbd6c718b80df793e3d936ceb9a8244
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2018-11-07 11:21:02 -0500

    Paneau

---
+++ Assets/Scripts/ToucheEnnemi.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ToucheEnnemi : MonoBehaviour {
+
+    public GameObject paneau; // paneau rouge 
+    public Animator animPaneau;
+    
+
+    void Desactif () {
+
+       // animPaneau = paneau.GetComponent<Animator>(); //on assigne à la variable le composant Rigidbody d'un objet 3D
+        gameObject.SetActive(false);
+    }
+	
+
+	void Update () {
+        //animPaneau.SetBool("EnnemieTouche", true);
+
+    }
+
+
+}
