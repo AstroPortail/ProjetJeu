714c37328afe6299c43eb7a61ea01578eb0bd54f
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2018-11-07 10:43:35 -0500

    hiverObjetsParticules

--- Assets/Scripts/ControlerPersonnage.cs
+++ Assets/Scripts/ControlerPersonnage.cs
@@ -33,15 +33,10 @@ public class ControlerPersonnage : MonoBehaviour
     public GameObject imageCitrouille;
     public GameObject imageChampi;
     public GameObject lesItems;
-    public MeshRenderer[] tableauMeshObjets;
 
     void Start()
     {
         DontDestroyOnLoad(gameObject);
-        positionRandomObjets();
-
-        StartCoroutine(positionRandomObjets());
-
         rbPerso = GetComponent<Rigidbody>();
         animPerso = GetComponent<Animator>();
        
@@ -114,14 +109,14 @@ public class ControlerPersonnage : MonoBehaviour
         if (infoCollision.gameObject.name == "cadeauAsset")
         {
             cadeauRamasse = true;
-            Countdown.totalTime += 15f;
+           Countdown.totalTime += 15f;
             Destroy(infoCollision.gameObject);
         }
 
         if (infoCollision.gameObject.name == "champisAsset")
         {
             champiRamasse = true;
-            Countdown.totalTime += 15f;
+           Countdown.totalTime += 15f;
             Destroy(infoCollision.gameObject);
         }
 
@@ -164,25 +159,6 @@ public class ControlerPersonnage : MonoBehaviour
         else
         {
             imageCitrouille.GetComponent<Image>().color = new Color32(255, 255, 255, 50);
-        }
-    }
-
-    IEnumerator positionRandomObjets()
-    {
-        for (var iEnum = 0; iEnum < tableauMeshObjets.Length ; iEnum++)
-        {
-            print(iEnum);
-            tableauMeshObjets[iEnum].enabled = false;
-        }
-        GameObject.Find("citrouilleAsset").transform.position = new Vector3(Random.Range(48f, -48f), 20, Random.Range(48f, -48f));
-        GameObject.Find("cadeauAsset").transform.position = new Vector3(Random.Range(48f, -48f), 20, Random.Range(48f, -48f));
-        GameObject.Find("champisAsset").transform.position = new Vector3(Random.Range(48f, -48f), 20, Random.Range(48f, -48f));
-        yield return new WaitForSeconds(4);
-
-        for (var iEnum2 = 0; iEnum2 < tableauMeshObjets.Length ; iEnum2++)
-        {
-            print(iEnum2);
-            tableauMeshObjets[iEnum2].enabled = true;
         }
     }
 
---
+++ Assets/Scripts/cadeau.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cadeau : MonoBehaviour {
+    public MeshRenderer[] tableauMeshObjets;
+
+    // Use this for initialization
+    void Start () {
+        StartCoroutine(positionRandomObjets());
+    }
+
+    IEnumerator positionRandomObjets()
+    {
+        for (var iEnum = 0; iEnum < tableauMeshObjets.Length; iEnum++)
+        {
+            print(iEnum);
+            tableauMeshObjets[iEnum].enabled = false;
+        }
+        //GameObject.Find("citrouilleAsset").transform.position = new Vector3(Random.Range(48f, -48f), 20, Random.Range(48f, -48f));
+        GameObject.Find("cadeauAsset").transform.position = new Vector3(Random.Range(48f, -48f), 20, Random.Range(48f, -48f));
+       // GameObject.Find("champisAsset").transform.position = new Vector3(Random.Range(48f, -48f), 20, Random.Range(48f, -48f));
+        yield return new WaitForSeconds(4);
+
+        for (var iEnum2 = 0; iEnum2 < tableauMeshObjets.Length; iEnum2++)
+        {
+            print(iEnum2);
+            tableauMeshObjets[iEnum2].enabled = true;
+        }
+    }
+}
