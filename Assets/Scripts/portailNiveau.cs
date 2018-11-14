beea9806c046884f79dd9f33674ad077ed7f4728
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2018-11-14 15:09:21 -0500

    portailChangeLaScene

---
+++ Assets/Scripts/portailNiveau.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class portailNiveau : MonoBehaviour {
+
+    public string sceneEnCours;
+
+    private void OnTriggerStay(Collider infoCollision)
+    {
+        sceneEnCours = SceneManager.GetActiveScene().name;
+
+        if (infoCollision.gameObject.name == "PrefabAstro")
+        {
+            int sceneAleatoire = Random.Range(0, 3);
+
+            if (sceneAleatoire == 0 && sceneEnCours != "Automne")
+            {
+
+                SceneManager.LoadScene("Automne");
+            }
+
+            if (sceneAleatoire == 1 && sceneEnCours != "HiverHelo")
+            {
+                //SceneManager.LoadScene("EteLaurence");
+                SceneManager.LoadScene("HiverHelo");
+            }
+
+            if (sceneAleatoire == 2 && sceneEnCours != "EteLaurence")
+            {
+                //SceneManager.LoadScene("HiverHelo");
+                SceneManager.LoadScene("EteLaurence");
+
+            }
+        }
+    }
+}
