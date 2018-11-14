15689985bff196bbbc83791d0b593219a6211b1d
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2018-11-14 16:22:26 -0500

    reEnnemis

---
+++ Assets/Scripts/gestionNavMeshEnnemis.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class gestionNavMeshEnnemis : MonoBehaviour {
+    //Animator ennemisAnim;
+    public static GameObject Cible;
+    // Use this for initialization
+    void Start () {
+        //ennemisAnim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Cible = GameObject.Find("PrefabAstro");
+        GetComponent<NavMeshAgent>().SetDestination(Cible.transform.position);
+        // Ligne à de code à décommenter lorsque le lapin se déplacera correctement. Elle permet d'activer l'animation de course.
+        //ennemisAnim.SetFloat("vitesse", GetComponent<NavMeshAgent>().velocity.magnitude);
+    }
+}
