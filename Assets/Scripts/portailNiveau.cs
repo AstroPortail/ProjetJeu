8fe2eef8f298c8924093112c2b06ebe768a940c7
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2018-11-14 19:07:36 -0500

    portailCondition

--- Assets/Scripts/portailNiveau.cs
+++ Assets/Scripts/portailNiveau.cs
@@ -7,12 +7,51 @@ using UnityEngine.UI;
 public class portailNiveau : MonoBehaviour {
 
     public string sceneEnCours;
+   // public GameObject ObjetDuNiveau;
+    public bool estTrouve = false;
+
+    public Image cadeau;
+    public Image champi;
+    public Image citrouille;
+
+    private void Start()
+    {
+        sceneEnCours = SceneManager.GetActiveScene().name;
+         
+    }
+
+    private void Update()
+    {
+        if (sceneEnCours == "Automne")
+        {
+            if (ControlerPersonnage.citrouilleRamasse == true)
+            {
+                estTrouve = true;
+                
+            }
+        }
+        else if (sceneEnCours == "HiverHelo")
+        {
+            if (ControlerPersonnage.cadeauRamasse == true)
+            {
+                estTrouve = true;
+                
+            }
+        }
+        else if (sceneEnCours == "EteLaurence")
+        {
+            if (ControlerPersonnage.champiRamasse == true)
+            {
+                estTrouve = true;
+                
+            }
+        }
+    }
 
     private void OnTriggerStay(Collider infoCollision)
     {
-        sceneEnCours = SceneManager.GetActiveScene().name;
 
-        if (infoCollision.gameObject.name == "PrefabAstro")
+        if (infoCollision.gameObject.name == "PrefabAstro" && estTrouve == true)
         {
             int sceneAleatoire = Random.Range(0, 3);
 
