954109e94c91852e540606c266a07089c9a4eb2e
Laurence <laurence.dodier@gmail.com>
2018-11-19 21:38:24 -0500

    AjustementProbleme
    Pas terminée encore des problemes avec ce que j'avais fait

---
+++ Assets/Scripts/ScriptsCamera/CameraPrePers.cs
@@ -0,0 +1,31 @@
+/* Script de camera qui affiche la camera a la premiere personne
+ * 
+ * @author Laurence Dodier
+ * @version 2018-11-14
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraPrePers : MonoBehaviour {
+
+    public GameObject cible; // le personnage
+    public GameObject camera; // la camera enfant du pivot
+
+    public float hauteurPivot; // hauteur de l'étiquette
+    public float positionXPivot; // position x de l'étiquette
+    public float positionZPivot; // position x de l'étiquette
+
+    // Position le pivot de la caméra au même endroit que le personnage + une certaine hauteur (prêt de la tête)
+    // le pivot tourne par le déplacement de la souris en X et Y
+    void Update()
+    {
+        transform.position = cible.transform.position + new Vector3(positionXPivot, hauteurPivot, positionZPivot);
+        transform.Rotate(-Input.GetAxis("Mouse Y"), Input.GetAxis("Mouse X"), 0);
+        //Annuler la rotation en Z
+        //transform.eulerAngles = new Vector3(transform.localEulerAngles.x, transform.localEulerAngles.y, 0);
+        //Caméra regarde le pivot
+        //camera.transform.LookAt(transform);
+         //rayCastcameraPivot.transform.LookAt(transform);
+    }
+}
--- Assets/Scripts/ToucheEnnemi.cs
+++ Assets/Scripts/ToucheEnnemi.cs
@@ -1,4 +1,10 @@
-﻿using System.Collections;
+/* Script désactive son gameObject
+ * 
+ * @author Laurence Dodier
+ * @version 2018-11-14
+ */
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,7 +12,7 @@ public class ToucheEnnemi : MonoBehaviour {
 
 
     void Desactif () {
-
+   
        // animPaneau = paneau.GetComponent<Animator>(); //on assigne à la variable le composant Rigidbody d'un objet 3D
         gameObject.SetActive(false);
     }
