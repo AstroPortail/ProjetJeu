ac0029a5f8509569d0aae507aaa1279f79958e5b
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2018-11-28 11:48:30 -0500

    sphereHiver
    navmesh

--- Assets/Scripts/AISphere.cs
+++ Assets/Scripts/AISphere.cs
@@ -6,20 +6,20 @@ using UnityEngine.AI;
 public class AISphere : MonoBehaviour {
     public Vector3 cibleRandom;
 
-
     private void Start()
     {
-        cibleRandom = new Vector3(Random.Range(2f, 93f), 8, Random.Range(2f, 93f));
+        cibleRandom = new Vector3(Random.Range(2f, 93f), 25, Random.Range(2f, 93f));
     }
     // Update is called once per frame
     void Update () {
 
         GetComponent<NavMeshAgent>().SetDestination(cibleRandom);
+        
         print(cibleRandom);
 
-        if(GetComponent<NavMeshAgent>().velocity.magnitude == 0)
+        if(GetComponent<NavMeshAgent>().velocity.magnitude <= 0.1f)
         {
-            cibleRandom = new Vector3(Random.Range(2f, 93f), 8, Random.Range(2f, 93f));
+            cibleRandom = new Vector3(Random.Range(2f, 93f), 25, Random.Range(2f, 93f));
         }
     }
 }
