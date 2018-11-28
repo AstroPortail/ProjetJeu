432bc901cda04e450be2be2e332b95991b90c4b3
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2018-11-28 11:03:39 -0500

    sphere

---
+++ Assets/Scripts/AISphere.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AISphere : MonoBehaviour {
+    public Vector3 cibleRandom;
+
+
+    private void Start()
+    {
+        cibleRandom = new Vector3(Random.Range(2f, 93f), 8, Random.Range(2f, 93f));
+    }
+    // Update is called once per frame
+    void Update () {
+
+        GetComponent<NavMeshAgent>().SetDestination(cibleRandom);
+        print(cibleRandom);
+
+        if(GetComponent<NavMeshAgent>().velocity.magnitude == 0)
+        {
+            cibleRandom = new Vector3(Random.Range(2f, 93f), 8, Random.Range(2f, 93f));
+        }
+    }
+}
