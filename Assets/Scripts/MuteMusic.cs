ae0375be75782dd04715041d879560de53c49fb9
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2018-12-05 10:27:06 -0500

    dfsdfs

---
+++ Assets/Scripts/MuteMusic.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MuteMusic : MonoBehaviour {
+    AudioSource audioSource;
+    public static bool sonMute;
+
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+        sonMute = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    public void MuteMusique()
+    {
+        if (!audioSource.isPlaying)
+        {
+            audioSource.Play();
+        } else
+        {
+            audioSource.Pause();
+            sonMute = true;
+        }
+    }
+}
---
+++ Assets/Scripts/VerifSon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerifSon : MonoBehaviour {
+    AudioSource audioSource;
+
+    // Use this for initialization
+    void Start () {
+		if (MuteMusic.sonMute ==true) {
+
+            audioSource.Pause();
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
