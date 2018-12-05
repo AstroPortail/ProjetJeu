cc9b6f45a8a098eb0d3e651f16f3f0c3fa26ee7f
AstroPortail <43211553+AstroPortail@users.noreply.github.com>
2018-12-05 10:44:31 -0500

    ennemisInstance

--- Assets/Scripts/instEnnemis.cs
+++ Assets/Scripts/instEnnemis.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class instEnnemis : MonoBehaviour {
+public class instEnnemis : MonoBehaviour
+{
+    // array contenant les ennemis à invoquer. (1x elephants, 3x lapins, 3x ours).
+    public GameObject[] arrayEnnemis;
+    float nombreEnnemis = 0;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        // on invoque un nouvel ennemi toutes les 2 secondes.
+        if(nombreEnnemis < 18)
+        {
+            InvokeRepeating("InvoqueEnnemis", 0f, 5f);
+        }
+        
+    }
+
+
+    public void InvoqueEnnemis()
+    {
+        // on choisit au hasard un index d'ennemi dans le tableau des ennemis.
+        int indexEnnemis = Random.Range(0, arrayEnnemis.Length - 1);
+
+        // on invoque un nouvel ennemi qui correspond avec l'index.
+        var nouvelEnnemis = Instantiate(arrayEnnemis[indexEnnemis]);
+
+        // on l'active.
+        nouvelEnnemis.SetActive(true);
+        nombreEnnemis++;
+    }
 }
